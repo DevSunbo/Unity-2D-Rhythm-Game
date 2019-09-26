7bad5618853d59249778949b37b273f50f58f54a
DevSunbo <30544967+DevSunbo@users.noreply.github.com>
2019-09-27 00:00:55 +0900

    구분선 작업 & 노트 오브젝트 생성 및 이동

---
+++ Unity_2D_Rhythm_Game/Assets/Scripts/NoteBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteBehavior : MonoBehaviour
+{
+    public int noteType; // 몇 번 노트인지
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.down * GameManager.instance.noteSpeed);
+    }
+}
